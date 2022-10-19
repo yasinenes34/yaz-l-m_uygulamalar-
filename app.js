4c30e4248fca0cde58dbf1b925685ecffca35778
yasin yıldırım <yasinnazmi1@gmail.com>
2022-10-19 14:37:02 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+require("dotenv/config");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const PORT = 5000;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const middleware = function (req, res, next) {
+    console.log("Yeni bir istek geldi.");
+    next();
+};
+
+const middlewarePost = function (req, res, next) {
+    console.log("Post iseği için istek gönderildi");
+    next();
+};
+app.use(middleware);
+
+app.get("/hello", function (req, res) {
+    res.json("Merhaba, GET isteği attınız");
+});
+app.post("/hello", middlewarePost, function (req, res) {
+    res.json("Merhaba, POST isteği attınız");
+});
+app.put("/hello", function (req, res) {
+    res.json("Merhaba, PUT isteği attınız");
+});
+app.delete("/hello", function (req, res) {
+    res.json("Merhaba, DELETE isteği attınız");
+});
+
+app.listen(PORT, () => {
+    console.log("Ready on http://localhost:" + PORT);
+});
